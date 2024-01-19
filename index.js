f42e36cea99fbcc38a73f2a2567c1c6532cd51cb
vladnomad <vlad.nmd@gmail.com>
2024-01-19 19:00:20 +0100

    7 – Basic types

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var string = "string";
-console.log(string);
+// let userName: string = "John"
+// userName = 5
+// userName.isInteger()
+// userName = "Lenny"
+// '', "", ``
+// 10, 0.5, 0.0001, -50, 4e10, NaN, Infinity
+// true, false
+var isBirthday = true;
+var age = 40;
+var userName = "River";
+if (isBirthday) {
+    console.log("Congrats, ".concat(userName, "! You have turned ").concat(age + 1, " today!"));
+}
