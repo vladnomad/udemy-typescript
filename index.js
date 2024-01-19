0c1f8e6254e7d864dfcdfbdbfe40f4aae1f2d927
vladnomad <vlad.nmd@gmail.com>
2024-01-19 19:58:33 +0100

    10 – Practice task

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
-// let userName: string = "John"
-// userName = 5
-// userName.isInteger()
-// userName = "Lenny"
-// '', "", ``
-// 10, 0.5, 0.0001, -50, 4e10, NaN, Infinity
-// true, false
-/*
-const isBirthday = true
-const age = 40
-const userName = "River"
-
-if (isBirthday) {
-    console.log(`Congrats, ${userName}! You have turned ${age + 1} years old today!`)
-}
- */
-var isBirthdayData = true;
-var ageData = 40;
-var userNameData = "River";
-/*
-function logBirthdayMessage (
-    isBirthday: boolean,
-    userName: string,
-    age: number
-): void {
-    if (isBirthday) {
-        console.log(`Congrats, ${userName.toUpperCase()}! You have turned ${age + 1} years old today!`)
-    }
-
-    return undefined
-}
- */
-var userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "River"}';
-var userObject = JSON.parse(userData);
-console.log(userObject);
-var logBirthdayMessage = function (isBirthday, age, userName) {
-    if (isBirthday) {
-        return "Congrats, ".concat(userName.toUpperCase(), "! You have turned ").concat(age + 1, " years old today!");
+var currencyRate = "1.05";
+var fetchCurrency = function (response) {
+    var data = JSON.parse(response);
+    return data;
+};
+function transferEurToUsd(available, amount, commission) {
+    if (available) {
+        var res = fetchCurrency(currencyRate) * amount * commission;
+        console.log(res);
     }
     else {
-        return "Error";
+        console.log("Сейчас обмен недоступен");
     }
-};
-logBirthdayMessage(isBirthdayData, ageData, userNameData);
-var salary;
-salary = 9000;
+}
+transferEurToUsd(true, 500, 1.05);
--- index.ts
+++ index.ts
@@ -1,62 +1,21 @@
-// let userName: string = "John"
+const currencyRate: string = "1.05"
 
-// userName = 5
-// userName.isInteger()
-
-// userName = "Lenny"
-
-// '', "", ``
-// 10, 0.5, 0.0001, -50, 4e10, NaN, Infinity
-// true, false
-/* 
-const isBirthday = true
-const age = 40
-const userName = "River"
-
-if (isBirthday) {
-    console.log(`Congrats, ${userName}! You have turned ${age + 1} years old today!`)
+const fetchCurrency = (response: string): number => {
+	const data: number = JSON.parse(response)
+	return data
 }
- */
 
-const isBirthdayData: boolean = true
-const ageData: number = 40
-const userNameData: string = "River"
-
-/* 
-function logBirthdayMessage (
-    isBirthday: boolean, 
-    userName: string, 
-    age: number
+function transferEurToUsd(
+    available: boolean, 
+    amount: number, 
+    commission: number
 ): void {
-    if (isBirthday) {
-        console.log(`Congrats, ${userName.toUpperCase()}! You have turned ${age + 1} years old today!`)
-    }
-
-    return undefined
-}
- */
-const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "River"}'
-
-const userObject: {
-    isBirthdayData: boolean, 
-    ageData: number, 
-    userNameData: string
-} = JSON.parse(userData)
-console.log(userObject)
-
-const logBirthdayMessage = (
-    isBirthday: boolean, 
-    age: number,
-    userName: string
-): string => {
-    if (isBirthday) {
-        return `Congrats, ${userName.toUpperCase()}! You have turned ${age + 1} years old today!`
-    } else {
-        return "Error"
-    }
+	if (available) {
+		let res: number = fetchCurrency(currencyRate) * amount * commission
+		console.log(res)
+	} else {
+		console.log("Сейчас обмен недоступен")
+	}
 }
 
-logBirthdayMessage(isBirthdayData, ageData, userNameData)
-
-let salary: number
-salary = 9000
+transferEurToUsd(true, 500, 1.05)
