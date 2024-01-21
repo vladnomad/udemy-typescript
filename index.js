ed80d642804e4028ec6266f35c59ca4b6a77697e
vladnomad <vlad.nmd@gmail.com>
2024-01-21 23:26:27 +0100

    12 – : null & undefined Types

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ function logBirthdayMessage(isBirthday, userName, age) {
     return createError("Error");
 }
 logBirthdayMessage(isBirthdayData, userNameData, ageData);
+var test = null;
+var test2 = null;
+// const test3: string = null
+// const test4: number = null 
+var test5 = undefined;
+var test6 = undefined;
+// const test7: string = undefined
+var smth;
+function getRndData() {
+    if (Math.random() < 0.5) {
+        return null;
+    }
+    else {
+        return "   Some data   ";
+    }
+}
+var data = getRndData();
+var trimmedData = data ? data.trim() : null;
