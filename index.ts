022d68b4ce6337013cf096394506e5b2b3ed0e91
vladnomad <vlad.nmd@gmail.com>
2024-01-28 20:09:36 +0100

    33-34 – Type Queries and Practice task

--- index.ts
+++ index.ts
@@ -1,73 +1,83 @@
 /* 
-// let some: any
-let some: unknown
-
-some = "string"
-
-// let data: string[] = some
-// data.find(e => e)
-
-// const someValue: any = 10
-const someValue: unknown = 10
-// someValue.method()
-
-function fetchData(data: unknown): void {
-	if (typeof data === "string") {
-		console.log(data.toLowerCase())
-	} else if (typeof data === "number") {
-		console.log(data.toFixed())
-	} else {
-
-	}
+interface Readings {
+	water: number;
+	electricity: number;
 }
  */
-const userData = '{"isA": true, "b": 23, "c": "string"}'
-
-function safeParse (s: string): unknown {
-	return JSON.parse(s)
+const readingsInspector = {
+	water: 200,
+	electricity: 350
 }
 
-const data = safeParse(userData)
+function compareReadings(readings: typeof readingsInspector): boolean {
+	const readingsUser = {
+		water: 200,
+		electricity: 350
+	}
 
-function transferData(d: unknown): void {
-	if (typeof d === "string") {
-		console.log(d.toLowerCase())
-	} else if (typeof d === "object" && d) {
-		console.log(data)
+	const waterIsEqual = readings.water === readingsUser.water
+	const electrictyIsEqual = readings.electricity === readingsUser.electricity
+
+	const readingsAreEqual = waterIsEqual && electrictyIsEqual
+
+	return readingsAreEqual
+}
+
+const PI = 3.14
+let PIClone: typeof PI
+
+
+// Practice task
+
+enum TypesOfMedia {
+	VIDEO = "video",
+	AUDIO = "audio"
+}
+
+enum FormatsOfMedia {
+	MP4 = ".mp4",
+	MOV = ".mov",
+	MKV = ".mkv",
+	FLV = ".flv",
+	WEBM = ".webM"
+}
+
+interface IMedia {
+	name: string;
+	type: TypesOfMedia;
+	format: FormatsOfMedia;
+	subtitles?: string;
+	timestamp?: unknown;
+}
+
+function playMedia({ name, type, format, subtitles, timestamp }: IMedia = {
+	name: "example",
+	type: TypesOfMedia.VIDEO,
+	format: FormatsOfMedia.MP4
+}): string {
+	let timestampLog: string
+
+	if (typeof timestamp === "string") {
+		timestampLog = timestamp
+	} else if (Array.isArray(timestamp)) {
+		timestampLog = timestamp.join(", ")
 	} else {
-		console.error("Invalid data")
+		timestampLog = "Unsupported type of timestamp"
 	}
+
+	console.log(
+		`The "${name}" ${format} is a ${type} type.
+		Timestamp: ${timestampLog}
+    	Subtitles: ${subtitles ?? "none"}`
+	)
+
+	return "Media has started"
 }
 
-transferData(data)
-
-try {
-	if (1) throw "error"
-} catch (e) {
-	console.log(typeof e)
-}
-// string
-
-try {
-	if (1) throw new Error("error")
-} catch (e) {
-	console.log(typeof e)
-}
-// object
-
-
-try {
-	if (1) throw new Error("error")
-} catch (e) {
-	if (e instanceof Error) {
-		console.log(e.message)
-	} else if (typeof e === "string") {
-		console.log(e)
-	}
-}
-
-type T0 = any | unknown
-type T1 = number | unknown
-
-type T2 = any & unknown
-type T3 = number & unknown
+playMedia({
+	name: "WoW",
+	type: TypesOfMedia.VIDEO,
+	format: FormatsOfMedia.FLV,
+	subtitles: "hmhmhm hmhmhm doh",
+	timestamp: ["4:30", "5:40"]
+})
