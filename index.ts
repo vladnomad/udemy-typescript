dc04ee1bcebb1770d15c63fbb24c7205b99b2e3b
vladnomad <vlad.nmd@gmail.com>
2024-01-28 18:11:08 +0100

    32 – Unknown type

--- index.ts
+++ index.ts
@@ -1,29 +1,73 @@
-enum Directions {
-	TOP = "top",
-	RIGHT = "right",
-	BOTTOM = "bottom",
-	LEFT = "left"
+/* 
+// let some: any
+let some: unknown
+
+some = "string"
+
+// let data: string[] = some
+// data.find(e => e)
+
+// const someValue: any = 10
+const someValue: unknown = 10
+// someValue.method()
+
+function fetchData(data: unknown): void {
+	if (typeof data === "string") {
+		console.log(data.toLowerCase())
+	} else if (typeof data === "number") {
+		console.log(data.toFixed())
+	} else {
+
+	}
+}
+ */
+const userData = '{"isA": true, "b": 23, "c": "string"}'
+
+function safeParse (s: string): unknown {
+	return JSON.parse(s)
 }
 
-enum TimingFunctions {
-	EASE = "ease",
-	EASE_IN = "ease-in",
-	// `${EASE}-in` – error, only numbers
-	EASE_OUT = "ease-out",
-	EASE_IN_OUT = "ease-in-out",
-	LINEAR = "linear"
-}
+const data = safeParse(userData)
 
-enum Durations {
-	ONE = 1,
-	TWO = 2,
-	THREE = ONE + TWO
-}
-
-function frame(element: string, direction: Directions, timingFunction: TimingFunctions, duration: Durations): void {
-	if (direction === Directions.RIGHT) {
-		console.log(element, timingFunction, duration)
+function transferData(d: unknown): void {
+	if (typeof d === "string") {
+		console.log(d.toLowerCase())
+	} else if (typeof d === "object" && d) {
+		console.log(data)
+	} else {
+		console.error("Invalid data")
 	}
 }
 
-frame("elementID", Directions.RIGHT, TimingFunctions.EASE, Durations.THREE)
+transferData(data)
+
+try {
+	if (1) throw "error"
+} catch (e) {
+	console.log(typeof e)
+}
+// string
+
+try {
+	if (1) throw new Error("error")
+} catch (e) {
+	console.log(typeof e)
+}
+// object
+
+
+try {
+	if (1) throw new Error("error")
+} catch (e) {
+	if (e instanceof Error) {
+		console.log(e.message)
+	} else if (typeof e === "string") {
+		console.log(e)
+	}
+}
+
+type T0 = any | unknown
+type T1 = number | unknown
+
+type T2 = any & unknown
+type T3 = number & unknown
