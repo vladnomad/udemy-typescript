2048365a36569220a92aaa7f8057960b1285982d
vladnomad <vlad.nmd@gmail.com>
2024-02-02 23:50:40 +0100

    46 – Generics

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ This course, authored by Ivan Petrichenko, offers an in-depth exploration of Typ
 
 ### ![100%](https://geps.dev/progress/100) I. Foundations 
 
-### ![0%](https://geps.dev/progress/0) II. Generics & type manipulations
+### ![5%](https://geps.dev/progress/5) II. Generics & type manipulations
 
 ### ![0%](https://geps.dev/progress/0) III. Classes
 
---
+++ lectures/46-generics.ts
@@ -0,0 +1,39 @@
+// @ts-nocheck
+function processingData<T>(data: T): T {
+	// ... 
+
+	return data
+}
+
+let res1 = processingData(1)
+let res2 = processingData("1")
+
+const num = "10"
+
+const res3  = processingData<number>(num)
+
+interface PrintUK {
+	design: number
+}
+
+interface PrintES {
+	design: string
+}
+
+interface Print<T> {
+	design: T
+}
+
+const somePrint: Print<string> = {
+	design: "ten"
+}
+
+const someOtherPrint: Print<number> = {
+	design: 10
+}
+
+Array<T>
+
+RefferalSystem<UserID, UserReferral>
+
+T  U  V  S  P  K/V
