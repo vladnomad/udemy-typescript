168e8300c8049fee713e7471debb83d742473138
vladnomad <vlad.nmd@gmail.com>
2024-02-14 11:19:02 +0100

    85 – Decorators in TypeScript version 5.+

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ This course, authored by Ivan Petrichenko, offers an in-depth exploration of Typ
 
 ### ![100%](https://geps.dev/progress/100) III. Classes
 
-### ![20%](https://geps.dev/progress/20) IV. Decorators & configuration
+### ![25%](https://geps.dev/progress/25) IV. Decorators & configuration
 
 ## What the Course Covers
 
--- index.js
+++ index.js
@@ -1,37 +1,94 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-let Car = class Car {
-    fuel = "50%";
-    open;
-    freeSpace;
-    isOpen() {
-        console.log(this.fuel);
-        return this.open ? "Open" : "Closed";
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
     }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
 };
-Car = __decorate([
-    closeCar
-], Car);
-function closeCar(constructor) {
-    // constructor.prototype.open = false
-    return class extends constructor {
-        open = false;
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+let Car = (() => {
+    let _classDecorators = [toggleLockStatus(false), changeFuelLevel(95)];
+    let _classDescriptor;
+    let _classExtraInitializers = [];
+    let _classThis;
+    var Car = class {
+        static { _classThis = this; }
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+            Car = _classThis = _classDescriptor.value;
+            if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+            __runInitializers(_classThis, _classExtraInitializers);
+        }
+        fuel = "50%";
+        lockStatus;
+        freeSpace;
+        isLocked() {
+            console.log(this.fuel);
+            return this.lockStatus ? "Locked" : "Unlocked";
+        }
+    };
+    return Car = _classThis;
+})();
+function toggleLockStatus(status) {
+    return (target, context) => {
+        return class extends target {
+            lockStatus = status;
+        };
     };
 }
-const car = new Car();
-console.log(car.isOpen());
-// addFuel(closeCar(car)).isOpen()
-/*
-function addFuel(car: Car) {
-    car.fuel = "100%"
-    console.log("addFuel")
-    return car
+function changeFuelLevel(level) {
+    return (target, context) => {
+        return class extends target {
+            fuel = `${level}%`;
+        };
+    };
 }
- */
-// console.log(closeCar(myCar).isOpen())
-// console.log(addFuel(closeCar(myCar)).isOpen())
+/*
+
+function toggleLockStatus(status: boolean) {
+    return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+        return class extends constructor {
+            lockStatus = status;
+        }
+    }
+}
+
+function changeFuelLevel(level: number) {
+    return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+        return class extends constructor {
+            fuel = `${level}%`;
+        }
+    }
+}
+
+*/
+const car = new Car();
+console.log(car.isLocked());
